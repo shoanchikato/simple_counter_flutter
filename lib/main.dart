9581d0b4dc24338c0794312b0afb294d89b4fe3c
Shoan Chikato <sjchikato@gmail.com>
2021-03-29 19:00:43 +0200

    add flutter_bloc to the dependencies and logic
    - add flutter_bloc dependency
    - add simple logic for counter to work using a cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+}
 
 void main() {
   runApp(MyApp());
@@ -8,7 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CounterPage(),
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: CounterPage(),
+      ),
     );
   }
 }
@@ -22,13 +32,20 @@ class CounterPage extends StatelessWidget {
         title: Text('Counter'),
       ),
       body: Center(
-        child: Text(
-          '0',
-          style: theme.textTheme.headline1,
+        child: BlocBuilder<CounterCubit, int>(
+          builder: (context, state) {
+            return Text(
+              '$state',
+              style: theme.textTheme.headline1,
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          // BlocProvider.of<CounterCubit>(context).increment();
+          context.read<CounterCubit>().increment();
+        },
         child: Icon(Icons.add),
       ),
     );
