da4edcf6b1083b8b170bee8e7eed29e9a7501979
Shoan Chikato <sjchikato@gmail.com>
2021-03-29 19:41:51 +0200

    refactor and start using convenience methods
    - start using convenience methods for accessing state and emitting
      events
    - context.read with add for emitting new events
    - context.watch for reading state, with CAUTION, to localize inside
      Builder widget or in smaller separate widget.
    - context.select can be used to retrieve part of a state
      and react to changes only when the selected part changes.
    
      final isPositive = context.select((CounterBloc b) => b.state >= 0);
    
      from: https://pub.dev/packages/flutter_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,22 @@ class CounterPage extends StatelessWidget {
         title: Text('Counter'),
       ),
       body: Center(
-        child: CounterText(),
+        // child: CounterText(),
+        child: Builder(
+          builder: (context) {
+            // NB: Part 2: if you want to use [context.watch()] with a widget hierachy
+            // make sure that is wrapped inside a Builder widget so that ONLY
+            // that part of the widget is rebuild when state changes
+            // NB: also checkout [context.select]
+            
+            final count = context.watch<CounterBloc>().state;
+            final theme = Theme.of(context);
+            return Text(
+              '$count',
+              style: theme.textTheme.headline1,
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -61,13 +76,16 @@ class CounterText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return BlocBuilder<CounterBloc, int>(
-      builder: (context, state) {
-        return Text(
-          '$state',
-          style: theme.textTheme.headline1,
-        );
-      },
+    // NB: PART 1: if you are going to use this approach of accessing state try to
+    // scope this as low as possible, so that only the widgets that need to be
+    // changed get rebuilt, because each time the state counter changes
+    // the all the widget(s) in the widget with [context.watch()] will be
+    // rebuilt/re-rendered
+
+    final count = context.watch<CounterBloc>().state;
+    return Text(
+      '$count',
+      style: theme.textTheme.headline1,
     );
   }
 }
